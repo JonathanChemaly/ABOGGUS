161e8cfcbf39d12cf1f730d4c978ba22566358e3
CameronC123 <cdang1234@gmail.com>
2023-01-31 16:53:20 -0500

    Moved folders
    Moved various folders into Resources
    Made volume start at 0.25
    Created folders for scripts

--- Assets/Scripts/Volume/VolumeSlider.cs
+++ Assets/Scripts/Volume/VolumeSlider.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour
 {
     public Slider slider;
+    private float initialVolume = 0.25f;
 
     public void Start()
     {
         slider.onValueChanged.AddListener(delegate { AdjustVolume(); });
+        slider.value = initialVolume;
     }
     public void AdjustVolume()
     {
