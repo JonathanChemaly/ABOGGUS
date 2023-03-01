607b05b23f9f7db914592783a463d2add0418943
CameronC123 <cdang1234@gmail.com>
2023-03-01 18:26:22 -0500

    FIXED Sprint bug and Camera/Movement when transitioning to boss scene

--- Assets/Scripts/Camera/Rotator.cs
+++ Assets/Scripts/Camera/Rotator.cs
@@ -4,10 +4,4 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public static float cameraYRot;
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-    }
 }
--- Assets/Scripts/PlayerObjects/States/PlayerFacingBackRight.cs
+++ Assets/Scripts/PlayerObjects/States/PlayerFacingBackRight.cs
@@ -12,13 +12,13 @@ namespace ABOGGUS.PlayerObjects
 
         public PlayerFacingBackRight(PlayerController playerController)
         {
-            yRot = Rotator.cameraYRot + yRotOffset;
+            yRot = ThirdPersonCameraController.cameraYRot + yRotOffset;
             this.physicalGameObject = playerController.GetGameObject();
             this.physicalGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, yRot, 0));
         }
         public void Move()
         {
-            yRot = Rotator.cameraYRot + yRotOffset;
+            yRot = ThirdPersonCameraController.cameraYRot + yRotOffset;
             physicalGameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, yRot, 0));
             Vector3 target = physicalGameObject.transform.position + physicalGameObject.transform.forward * PlayerController.speed;
             physicalGameObject.transform.localPosition = Vector3.MoveTowards(physicalGameObject.transform.localPosition, target, PlayerController.speed);
