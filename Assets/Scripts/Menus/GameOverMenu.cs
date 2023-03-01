e27a63bc94b85c0e4c03d1f30cfdf8226e725583
Curtis High <high.66@osu.edu>
2023-03-01 17:25:45 -0500

    Updated game over menu

--- Assets/Scripts/Menus/GameOverMenu.cs
+++ Assets/Scripts/Menus/GameOverMenu.cs
@@ -1,31 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 using ABOGGUS.Gameplay;
-using ABOGGUS.SaveSystem;
-using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace ABOGGUS.Menus
 {
     public class GameOverMenu : MonoBehaviour
     {
         public GameObject gameOverMenu;
+        public Image gameOverImage;
         public static bool isPaused;
+        private bool openMenu;
+        private static float ogVolume;
+
+        public const float fadeDuration = 2.0f;
+        private const float alphaInc = fadeDuration == 0.0f ? 1.0f : 1.0f / fadeDuration;
 
         // Start is called before the first frame update
         void Start()
         {
             gameOverMenu.SetActive(false);
+            Color clearBlack = Color.black;
+            clearBlack.a = 0;
+            gameOverImage.color = clearBlack;
             isPaused = false;
+            openMenu = false;
+
+            // bring this canvas to front
+            transform.Find("GameOverCanvas").GetComponent<Canvas>().sortingOrder = 10;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (isPaused) OpenGameOverMenu();
+            if (isPaused && !openMenu)
+            {
+                FadeToBlack();
+            }
+            else if (openMenu)
+            {
+                OpenGameOverMenu();
+            }
         }
 
         public static void ActivateGameOver()
@@ -35,6 +50,8 @@ namespace ABOGGUS.Menus
             Cursor.visible = true;
             isPaused = true;
             GameController.PauseGame();
+            ogVolume = AudioListener.volume;
+            AudioListener.volume = 0;
             //GameController.player.debug = false;
         }
 
@@ -43,22 +60,46 @@ namespace ABOGGUS.Menus
             gameOverMenu.SetActive(true);
         }
 
+        private void FadeToBlack()
+        {
+            // fade to black
+            gameOverImage.gameObject.SetActive(true);
+            Color newColor = gameOverImage.color;
+            //Debug.Log("Current color: " + newColor);
+            float t = Time.deltaTime;
+            if (newColor.a + alphaInc*t > 1)
+            {
+                newColor.a = 1;
+                openMenu = true;
+            }
+            else newColor.a += alphaInc*t;
+            gameOverImage.color = newColor;
+        }
+
         public void GoToLobby()
         {
             GameController.ResetPlayerHealth();
+            RestoreVolume();
             GameController.Respawn();
         }
 
         public void GoToMainMenu()
         {
             GameController.ResetPlayerHealth();
+            RestoreVolume();
             GameController.ChangeScene("Going to main menu from game over menu.", GameConstants.SCENE_MAINMENU, true);
         }
 
         public void QuitGame()
         {
             GameController.ResetPlayerHealth();
+            RestoreVolume();
             GameController.QuitGame("Quit from game over menu.");
+        }
+
+        private void RestoreVolume()
+        {
+            AudioListener.volume = ogVolume;
         }
     }
 }
