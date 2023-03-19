9f5e63d400a27ce1076fedb7e533376fefad92f0
Curtis High <high.66@osu.edu>
2023-03-19 19:10:12 -0400

    Revert "Revert "Tile puzzle functional""
    This reverts commit 71f5b8af521cf203533fb3a26b8cfdc6e8d42296.

--- Assets/Scripts/PuzzleScripts/TilePuzzle.cs
+++ Assets/Scripts/PuzzleScripts/TilePuzzle.cs
@@ -39,7 +39,21 @@ namespace ABOGGUS.Interact.Puzzles
         public void SetPosition(Vector2 newPos)
         {
             pos = newPos;
-            this.transform.position = new Vector3(TilePuzzleManager.TILE_PUZZLE_SPACE*newPos.x, 1, TilePuzzleManager.TILE_PUZZLE_SPACE*newPos.y);
+            this.transform.position = new Vector3(tpm.tileSpace*newPos.x, 1, tpm.tileSpace*newPos.y);
+        }
+
+        public void ApplyTextureFromOrder(Texture2D texture)
+        {
+            // create sprite
+            int rectLength = texture.width / tpm.numTilesLong;
+            int xStart = orderNum % tpm.numTilesLong, yStart = orderNum / tpm.numTilesLong;
+            Rect rect = new Rect(rectLength * xStart, rectLength * yStart, rectLength, rectLength);
+            Sprite sprite = Sprite.Create(texture, rect, Vector2.one * 0.5f);
+
+            // apply to Sprite child of tile
+            GameObject spriteObj = transform.Find("Sprite").gameObject;
+            spriteObj.transform.localScale /= (rectLength / 100.0f);
+            spriteObj.GetComponent<SpriteRenderer>().sprite = sprite;
         }
     }
 }
