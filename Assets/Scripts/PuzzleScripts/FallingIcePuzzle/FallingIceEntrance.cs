8f00a0b0b19715adaee71be31a87a72cfcf054e7
Jonathan <jonathanchemaly@gmail.com>
2023-04-06 15:10:07 -0400

    Implemented Ice Falling puzzle into winter room

--- Assets/Scripts/PuzzleScripts/FallingIcePuzzle/FallingIceEntrance.cs
+++ Assets/Scripts/PuzzleScripts/FallingIcePuzzle/FallingIceEntrance.cs
@@ -4,39 +4,10 @@ using UnityEngine;
 
 public class FallingIceEntrance : MonoBehaviour
 {
-    private Vector3 startPoistion;
-    private Vector3 tempPoistion;
-    private float timer = 20f;
-    private bool moved = false;
-
-    void Start()
-    {
-        startPoistion = transform.position;
-        tempPoistion = startPoistion + new Vector3(0f, 20f, 0f);
-    }
-    private void FixedUpdate()
-    {
-
-        if (moved)
-        {
-            timer -= Time.deltaTime;
-        }
-        if (timer < 20f && timer > 19f)
-        {
-            transform.position = tempPoistion;
-        }
-        if (timer <= 0)
-        {
-            moved = false;
-            timer = 10f;
-            transform.position = startPoistion;
-        }
-    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            moved = true;
             FallingIceFloorPuzzle.CreateFloor();
         }
     }
--- Assets/Scripts/PuzzleScripts/FallingIcePuzzle/FallingIceFloorPuzzle.cs
+++ Assets/Scripts/PuzzleScripts/FallingIcePuzzle/FallingIceFloorPuzzle.cs
@@ -14,11 +14,13 @@ public class FallingIceFloorPuzzle : MonoBehaviour
     public static Vector3 startPoint;
     private static GameObject fall;
     private static GameObject wall;
+    private static GameObject exit;
     private static int row;
     private static int col;
     private static int times = 0;
-    private int[,] grid;
     private static bool puzzleStart = false;
+    private static float timer = 10f;
+    private static bool once = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,7 @@ public class FallingIceFloorPuzzle : MonoBehaviour
         wall = iceWall;
         row = rows;
         col = columns;
+        exit = iceExit;
 
         for (int c = 0; c < col; c++)
         {
@@ -42,13 +45,14 @@ public class FallingIceFloorPuzzle : MonoBehaviour
                 }
             }
         }
-        Instantiate(iceExit, startPoint + new Vector3((col / 2) * 4f, 0f, -4f), Quaternion.identity);
+        
     }
 
     public void FixedUpdate()
     {
         if (puzzleStart)
         {
+            timer -= Time.deltaTime;
             iceFloors = GameObject.FindGameObjectsWithTag("iceFloor");
             if (iceFloors.Length == 0)
             {
@@ -56,38 +60,47 @@ public class FallingIceFloorPuzzle : MonoBehaviour
                 Destroy(GameObject.Find("iceExit"));
                 puzzleStart = false;
             }
+            if (timer < 0f)
+            {
+                timer = 10f;
+                once = true;
+            }
         }
     }
 
     public static void CreateFloor()
     {
-        times++;
-        Debug.Log(times);
-
-        for (int c = 1; c < col-1; c++)
+        if (once)
         {
-            for (int r = 1; r < row-1; r++)
-            {                
-                if (r == 1 || r == row - 2 || c == 1 || c == col - 2)
+            times++;
+            Debug.Log(times);
+            Instantiate(exit, startPoint + new Vector3((col / 2) * 4f, 0f, -4f), Quaternion.identity);
+            for (int c = 1; c < col - 1; c++)
+            {
+                for (int r = 1; r < row - 1; r++)
                 {
-                    Instantiate(fall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
-                }
-                else if(r == row / 2 && c == col / 2)
-                {
-                    //do nothing
-                }
-                else if (Random.Range(0, 4) == 0)
-                {
-                    Instantiate(wall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
-                }
-                else
-                {
-                    Instantiate(fall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
-                }
+                    if (r == 1 || r == row - 2 || c == 1 || c == col - 2)
+                    {
+                        Instantiate(fall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
+                    }
+                    else if (r == row / 2 && c == col / 2)
+                    {
+                        //do nothing
+                    }
+                    else if (Random.Range(0, 4) == 0)
+                    {
+                        Instantiate(wall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(fall, startPoint + new Vector3(c * 4f, 0f, r * 4f), Quaternion.identity);
+                    }
 
+                }
             }
+            puzzleStart = true;
+            once = false;
         }
-        puzzleStart = true;
     }
 
 }
