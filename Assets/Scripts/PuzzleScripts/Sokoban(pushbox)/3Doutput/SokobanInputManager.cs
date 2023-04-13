a61f253108eb2791dfb2a31c3eec7117334a42a1
phangen <ph.hangen@gmail.com>
2023-04-12 21:14:55 -0400

    Sokoban stuff

---
+++ Assets/Scripts/PuzzleScripts/Sokoban(pushbox)/3Doutput/SokobanInputManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ABOGGUS.Input;
+
+namespace ABOGGUS.Interact.Puzzles.Sokoban
+{
+    public class SokobanInputManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private static InputActions InputScheme;
+        private bool additiveLoaded = false;
+
+        [SerializeField]
+        [Tooltip("Movement Controller that we intialize")]
+        private SokobanMovementController sokobanMovementController;
+
+        //[SerializeField]
+        //[Tooltip("Material to apply to the sky")]
+        //private Material skyBoxMaterial;
+
+        //private Material oldSkyBoxMaterial;
+        
+        void Start()
+        {
+            //Skybox changes
+            //oldSkyBoxMaterial = RenderSettings.skybox;
+            //RenderSettings.skybox = skyBoxMaterial;
+
+            InputManager[] imList = GameObject.FindObjectsOfType<InputManager>();
+            if(imList.Length > 0)
+            {
+                InputScheme = imList[0].InputScheme;
+                //disable all player input
+                InputScheme.Player.Disable();
+                additiveLoaded = true;
+            }
+            else
+            {
+                InputScheme = new Input.InputActions();
+                GenerateSokoban[] genSokList = GameObject.FindObjectsOfType<GenerateSokoban>();
+                genSokList[0].enabled = true;
+            }
+            sokobanMovementController.InitializeInput(InputScheme);
+
+            
+        }
+
+        private void OnDestroy()
+        {
+            if (additiveLoaded)
+            {
+                sokobanMovementController.DeallocateInput(InputScheme);
+                InputScheme.Player.Enable();
+                InputScheme.Sokoban.Disable();
+                
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (additiveLoaded)
+            {
+                sokobanMovementController.DeallocateInput(InputScheme);
+                InputScheme.Player.Enable();
+                InputScheme.Sokoban.Disable();
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/PuzzleScripts/Sokoban(pushbox)/CellTypes/SokobanCell.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ABOGGUS.Interact.Puzzles.Sokoban.SokobanStructs;
+
+namespace ABOGGUS.Interact.Puzzles.Sokoban
+{
+    public abstract class SokobanCell
+    {
+        public abstract List<SokobanCell> AdjacentList { get; }
+
+        public abstract bool IsFloor();
+
+        public abstract bool PlayerIsHere { get; set; }
+
+    }
+}
+
