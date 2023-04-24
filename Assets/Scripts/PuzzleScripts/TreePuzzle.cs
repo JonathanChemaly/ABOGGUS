248a54e9cbe179a21882f7b2026ee1de8e7ae512
Curtis High <high.66@osu.edu>
2023-04-24 18:54:59 -0400

    tree puzzle loading

--- Assets/Scripts/PuzzleScripts/TreePuzzle.cs
+++ Assets/Scripts/PuzzleScripts/TreePuzzle.cs
@@ -15,6 +15,10 @@ namespace ABOGGUS.Interact.Puzzles
         [SerializeField] private Interactable tree;
         private ParticleSystem particles;
 
+        public enum Status {DIRT, SPROUT, SAPLING, TREE, FINAL};
+        public static Status status = Status.DIRT;
+        private Status oldStatus = Status.DIRT;
+
         public static int latestRun = -1;
 
         // Start is called before the first frame update
@@ -25,6 +29,19 @@ namespace ABOGGUS.Interact.Puzzles
             sapling.InteractAction += InteractSapling;
             tree.InteractAction += InteractTree;
             particles = this.transform.Find("Particles").GetComponent<ParticleSystem>();
+        }
+
+        private void Update()
+        {
+            if (oldStatus != status)
+            {
+                if (temp == 1) InteractDirt();
+                if (temp == 2) InteractSprout();
+                if (temp == 3) InteractSapling();
+                if (temp == 4) InteractTree();
+
+                oldStatus = status;
+            }
         }
 
         private void UpdateRun()
